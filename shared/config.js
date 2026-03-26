dec7487654a0acf73258e40379b120005ef68b6e
Sphelele Chavula <143267537+Spheh123@users.noreply.github.com>
2026-03-26 14:26:09 +0200

    Initial commit

---
+++ shared/config.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZTrE4PuhIky5qaB1jmtoGuUyqVTWbg0Q",
+    authDomain: "streams-of-joy-jhb.firebaseapp.com",
+    projectId: "streams-of-joy-jhb",
+    storageBucket: "streams-of-joy-jhb.firebasestorage.app",
+    messagingSenderId: "13302946345",
+    appId: "1:13302946345:web:90f191028bdc8cc1f51a38"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
