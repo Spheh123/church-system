46c147fe33de6c153edff61d7587e6837cefdf99
Sphelele Chavula <143267537+Spheh123@users.noreply.github.com>
2026-03-28 19:54:55 +0200

    Update config.js

--- shared/config.js
+++ shared/config.js
@@ -1,11 +1,21 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBZTrE4puIHky5qaB1jmtoGuUyqVTWbg0Q",
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZTrE4PuhIky5qaB1jmtoGuUyqVTWbg0Q",
   authDomain: "streams-of-joy-jhb.firebaseapp.com",
   projectId: "streams-of-joy-jhb",
   storageBucket: "streams-of-joy-jhb.firebasestorage.app",
   messagingSenderId: "13302946345",
   appId: "1:13302946345:web:90f191028bdc8cc1f51a38"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const appConfig = {
   appName: "Streams of Joy Johannesburg CMS",
   supportEmail: "admin@streamsofjoyjhb.org",
